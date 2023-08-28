513387065ea2ad80a4463cd28a6f4ef3fb0886cf
agentsmith4197 <87205529+agentsmith4197@users.noreply.github.com>
2023-08-28 13:25:05 +0100

    Update 3-string_methods.py

--- list-dict_if/3-string_methods.py
+++ list-dict_if/3-string_methods.py
@@ -1,10 +1,10 @@
 myString = "python is a cool program, python is love, python is fun"
 python_count = myString.split().count("python")
-print(f'the word python appears {python_count} times \n ')
+print(python_count)
 capitalized = myString.title()
 print(f'{capitalized} \n ')
 
 target = "python"
 targeted = target.capitalize()
-capital = myString.replace(target, targeted, 1)
+capital = myString.replace(target, targeted)
 print(capital)
