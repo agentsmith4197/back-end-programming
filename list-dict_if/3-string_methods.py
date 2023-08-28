ff7346790006ee0edad5185923edb3a24345a0d9
agentsmith4197 <87205529+agentsmith4197@users.noreply.github.com>
2023-08-28 13:34:07 +0100

    Update 3-string_methods.py

--- list-dict_if/3-string_methods.py
+++ list-dict_if/3-string_methods.py
@@ -1,10 +1,10 @@
 myString = "python is a cool program, python is love, python is fun"
 python_count = myString.split().count("python")
 print(python_count)
-capitalized = myString.title()
+capitalized = myString.upper()
 print(f'{capitalized} \n ')
 
 target = "python"
-targeted = target.capitalize()
+targeted = target.upper()
 capital = myString.replace(target, targeted)
 print(capital)
